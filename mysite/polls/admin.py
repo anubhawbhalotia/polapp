6a49612b1a6b5917fea957830630dec6f3e58d92
anubhawbhalotia <bhalotia.anubhaw4@gmail.com>
2019-11-02 19:37:46 +0530

    improved was_published_recently

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -20,7 +20,15 @@ class ChoiceInline(admin.TabularInline):
 #         ('Date information',    {'fields': ['pub_date']}),
 #     ]
 
+# class QuestionAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None,                  {'fields': ['question_text']}),
+#         ('Date information',    {'fields': ['pub_date']}),
+#     ]
+#     inlines = [ChoiceInline]
+
 class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
     fieldsets = [
         (None,                  {'fields': ['question_text']}),
         ('Date information',    {'fields': ['pub_date']}),
